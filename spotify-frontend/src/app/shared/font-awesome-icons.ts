23b7a8d15d0b56c4801a21d4391247a5a97a375b
grigoriank8 <karogrigorian8@gmail.com>
2024-07-18 23:10:40 +0400

    front-end part

---
+++ spotify-frontend/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,8 @@
+import {IconDefinition} from "@fortawesome/free-regular-svg-icons";
+import {faCirclePlay, faHome, faSearch} from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faCirclePlay,
+  faHome,
+  faSearch
+]
